b2009c85055f6ce948b19f7cbfc5661afac5a7a3
Elizabeth Hayt <enhayt@gmail.com>
2021-05-07 09:05:03 -0700

    Initializr

---
+++ src/main/java/com/example/elizabethjesseimdb/ElizabethJesseImdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.elizabethjesseimdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElizabethJesseImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElizabethJesseImdbApplication.class, args);
+	}
+
+}
