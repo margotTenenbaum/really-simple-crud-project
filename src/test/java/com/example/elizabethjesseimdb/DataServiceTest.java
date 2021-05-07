ff54897c3922817c58bb90ac2a5d55302793b4c8
Elizabeth Hayt <enhayt@gmail.com>
2021-05-07 13:10:56 -0700

    Add tests for getMovieById

--- src/test/java/com/example/elizabethjesseimdb/DataServiceTest.java
+++ src/test/java/com/example/elizabethjesseimdb/DataServiceTest.java
@@ -11,12 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DataServiceTest {
     DataService dataService;
     List<Movie> testData;
+    Movie movie1;
 
     @BeforeEach
     void setup() {
         testData = new ArrayList<>();
         dataService = new DataService();
-        Movie movie1 = new Movie("The Royal Tenenbaums", "2001", "notNominated", "comedy");
+        movie1 = new Movie("The Royal Tenenbaums", "2001", "notNominated", "comedy");
         Movie movie2 = new Movie("Citizen Kane", "1941", "oscarWinner", "drama");
         Movie movie3 = new Movie("The Godfather", "1972", "oscarWinner", "drama");
         testData.add(movie1);
@@ -30,5 +31,20 @@ public class DataServiceTest {
     @Test
     void getMovies() {
         assertEquals(3, dataService.getMovies().size());
+    }
+
+    @Test
+    void getMovieByIdReturnsMovieById() {
+        Movie actual = dataService.getMovieById(0);
+        assertEquals(movie1, actual);
+    }
+
+    @Test
+    void addedMovieShouldBeInDataService() {
+        Movie expected = new Movie("Mortal Kombat", "2021", "notNominated", "action");
+        dataService.addMovie(expected);
+        Movie actual = dataService.getMovieById(3);
+        assertEquals(4, dataService.getMovies().size());
+        assertEquals(expected, actual);
     }
 }
